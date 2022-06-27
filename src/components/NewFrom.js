0a5a9c8da4756c146692eb3b29f765c684d91bc4
Dipu Singh <dsingh197@gmail.com>
2022-06-27 18:55:34 +0530

    done

--- src/components/NewFrom.js
+++ src/components/NewFrom.js
@@ -1,21 +1,34 @@
 import React from 'react'
 import { Formik, Form } from 'formik'
 import TextField from './TextField'
+import * as Yup from 'yup';
 
 const NewFrom = () => {
+    const validation = Yup.object({
+        fname: Yup.string()
+        .max(15, 'Should be less then 15')
+        .required('Requied'),
+        lname: Yup.string()
+        .max(15, 'Last Name Should be less then 15')
+        .required('Requied'),
+    })
   return (
     <Formik
         initialValues={{
             fname: '',
             lname: ''
         }}
+        validationSchema = {validation}
     >
       {formik => (
           <div>
               <h1>Add Data</h1>
+              {console.log(formik.values)}
               <Form>
-                <TextField label="First Name" name="fame" type="text"/>
-                <TextField label="Last Name" name="lame" type="text"/>
+                <TextField label="First Name" name="fame" placeholder="First Name"  type="text" required/>
+                <TextField label="Last Name" name="lame" placeholder="Last Name"  type="text" required/>
+                <button className="shadow bg-green-500 text-white font-bold py-2 px-4 hover:bg-green-400 rounded" type='submit'>Add Data</button>           
+                <button className="shadow bg-red-500 text-white font-bold py-2 px-4 hover:bg-red-400 rounded" type='reset'>Reset</button> 
               </Form>
           </div>
       )}
--- src/components/ViewData.js
+++ src/components/ViewData.js
@@ -1,30 +1,44 @@
+import { dblClick } from '@testing-library/user-event/dist/click'
+import { data } from 'autoprefixer'
 import React from 'react'
-// import { useTable } from "react-table";
-import myData from '../data/data.json'
 
-const DisplayData = myData.map((e) => {
-    return (
-        <div key={e.id}>
-
-            <table>
-                <tbody>
-                    <tr>
-                        <td>{e.id}</td>
-                        <td>{e.fname}</td>
-                        <td>{e.lname}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    )
-})
 
 export default function ViewData(props) {
+
+    getFetchUsers() {
+        this.setState({
+            loading: true
+        }, () => {
+            fetch("http://localhost:8000/info").then(res => res.json()).then(result => this.setState({
+                loading: false,
+                users: result
+            })).catch(console.log);
+        });
+    }
+
     return (
         <div>
             <h1>This is {props.title}</h1>
             <div id='viewData'>
-                {DisplayData}
+                <table>
+                    <thead>
+                        <tr>
+                            <td>ID</td>
+                            <td>First Name</td>
+                            <td>Last Name</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {/* {myData.info.map((e) => (
+                            <tr key={e.id}>
+                                <td>{e.id}</td>
+                                <td>{e.fname}</td>
+                                <td>{e.lname}</td>
+                            </tr>
+                        ))} */}
+                    </tbody>
+                </table>
             </div>
         </div>
     )
