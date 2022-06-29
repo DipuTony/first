343670f3e7b847f441b3daf830e646733e51442c
Dipu Singh <dsingh197@gmail.com>
2022-06-29 10:25:19 +0530

    change in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,17 @@ import { Link } from 'react-router-dom';
 export default function Header(props) {
     return (
         <div>
-            <header className="text-black-600 body-font">
-                <div className="container mx-auto flex flex-wrap p-4 m-0 flex-col md:flex-row items-center bg-blue-400 rounded">
+            <header className="text-black-600">
+                <div className="flex p-4 bg-blue-400">
                     <a className="flex title-font font-medium items-center text-gray-900 ">
-                       <div style={{color:'#8830f2'}}> <SiYoutubestudio  size={30}/></div>
+                        <div style={{ color: '#8830f2' }}> <SiYoutubestudio size={30} /></div>
                         <span className="ml-3 text-xl">{props.title}</span>
                     </a>
                     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                         <Link to="/" className="mr-5 hover:text-gray-900">Home</Link>
                         <Link to="/about" className="mr-5 hover:text-gray-900">About</Link>
                     </nav>
-                    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">{props.date}
-                    </button>
+                    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-0 md:mt-0">{props.date} </button>
                 </div>
             </header>
         </div>
