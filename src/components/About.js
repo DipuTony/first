63aa91aebfc2438adfe0958f74f8cfc65193930f
Dipu Singh <dsingh197@gmail.com>
2022-07-04 18:42:16 +0530

    abou me change

--- src/components/About.js
+++ src/components/About.js
@@ -3,15 +3,21 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            <section class="text-gray-600 body-font">
-                <div class="container px-5 py-24 mx-auto">
-                    <div class="lg:w-2/3 flex flex-col sm:items-center items-start mx-auto">
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="lg:w-2/3 flex flex-col sm:items-center items-start mx-auto">
                         
                         <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">About US</h1><br/>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa blanditiis quam veniam enim officiis eius nesciunt quo nam et nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem molestias, soluta cumque a tenetur laudantium, repellat corrupti quam, expedita nemo nisi veritatis optio neque minima. Vel blanditiis voluptates doloribus unde iste! Laborum, debitis velit?</p>
+
+
                     </div>
                 </div>
             </section>
+            <div className="flex justify-center text-center">
+                <h1 className='bg-blue-300 w-52 p-3'>Library Used</h1>                                   
+                <div className='justify-center text-center'> Hello</div>
+            </div>
         </div>
     )
 }
